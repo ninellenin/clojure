9a3fbd01db7d7f8dab29d187983901ee87148eb5
Sazonova Yuliya <yulia-saz3@yandex.ru>
2017-10-28 12:27:29 +0700

    Fixed tests

--- lab3/test/ru/nsu/fit/dt/sazonova/lazy_integral_test.clj
+++ lab3/test/ru/nsu/fit/dt/sazonova/lazy_integral_test.clj
@@ -4,8 +4,8 @@
         
 (deftest integrate-test
   (testing "Testing of function integrate."
-    (is (<= (Math/abs (- ((integrate constant approximate) 1.) 1.)) 0.001))
-    (is (<= (Math/abs (- ((integrate constant approximate) 2.) 2.)) 0.001))
-    (is (<= (Math/abs (- ((integrate line approximate) 1.) 0.5)) 0.001))
-    (is (<= (Math/abs (- ((integrate line approximate) 3.) 4.5)) 0.001))
+    (is (<= (Math/abs (- ((integrate constant) 1.) 1.)) 0.001))
+    (is (<= (Math/abs (- ((integrate constant) 2.) 2.)) 0.001))
+    (is (<= (Math/abs (- ((integrate line) 1.) 0.5)) 0.001))
+    (is (<= (Math/abs (- ((integrate line) 3.) 4.5)) 0.001))
     ))
